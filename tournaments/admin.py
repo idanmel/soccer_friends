9a150454c433d876b78484805d185836c047152b
idanmel <idanmel@gmail.com>
2017-12-30 22:02:41 +0200

    Added match model

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from tournaments.models import Tournament, Stage, Team
+from tournaments.models import Tournament, Stage, Team, Match
 
 # Register your models here.
 admin.site.register(Tournament)
 admin.site.register(Stage)
 admin.site.register(Team)
+admin.site.register(Match)
--- tournaments/models.py
+++ tournaments/models.py
@@ -23,3 +23,14 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
+
+class Match(models.Model):
+    stage = models.ForeignKey(Stage, related_name='matches', on_delete=models.CASCADE)
+    home_team = models.ForeignKey(Team, related_name='home_team_matches', on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Team, related_name='away_team_matches', on_delete=models.CASCADE)
+    home_goals = models.IntegerField(default=0)
+    away_goals = models.IntegerField(default=0)
+    finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{} - {}".format(self.home_team, self.away_team)
