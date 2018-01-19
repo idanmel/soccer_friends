0d75d00296bcf41c91f3beaff505a9fd89151b4a
idanmel <idanmel@gmail.com>
2018-01-19 19:15:40 +0200

    White list the heroku frontend app

--- soccer_friends/settings.py
+++ soccer_friends/settings.py
@@ -114,7 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
-    '127.0.0.1:8080'
+    '127.0.0.1:8080',
+    'https://soccer-friends-frontend.herokuapp.com'
 )
 
 # Internationalization
